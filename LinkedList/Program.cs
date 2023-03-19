5ac4f0c5e9ddcb4418fd55b4eed10953e1030896
MathPelicer <matheuspelicer58@gmail.com>
2023-03-18 21:06:46 -0300

    basic LinkedList data structure implementation

---
+++ LinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using LinkedList;
+
+MyLinkedList<int> l = new();
+l.Insert(3);
+l.Insert(7);
+l.Insert(1);
+l.Insert(3);
+l.Insert(6);
+l.Insert(9);
+l.Insert(24);
+
+l.Print();
+
+l.Remove(1);
+l.Remove(9);
+l.Remove(24);
+l.Print();
+
