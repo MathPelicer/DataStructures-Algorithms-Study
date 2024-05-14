43e709133c29701ce4b2520a5b835858a4ec62d8
MathPelicer <matheuspelicer58@gmail.com>
2024-05-13 23:15:25 -0300

    feat: implement remove by value feature in GO simply linked list

--- LinkedList/go/linkedlist.go
+++ LinkedList/go/linkedlist.go
@@ -1,4 +1,8 @@
-package linkedlist
+package main
+
+import (
+	"fmt"
+)
 
 type LinkedList struct {
 	len  int
@@ -19,23 +23,68 @@ func (linkedList *LinkedList) add(value int) {
 		return
 	}
 
-	// rest
+	curNode := linkedList.head
+
+	for curNode.next != nil {
+		curNode = curNode.next
+	}
+
+	curNode.next = &Node{value: value, next: nil}
+
+	linkedList.len++
 }
 
-func (linkedList *LinkedList) remove() {
+func (linkedList *LinkedList) remove(value int) {
+	if linkedList.head != nil && linkedList.head.value == value {
+		linkedList.head = linkedList.head.next
 
+		linkedList.len--
+		return
+	}
+
+	curNode := linkedList.head
+
+	for curNode.next != nil {
+		if curNode.next.value == value {
+			curNode.next = curNode.next.next
+
+			linkedList.len--
+			return
+		}
+		curNode = curNode.next
+	}
 }
 
 func search() {
 
 }
 
-func print() {
+func (linkedList *LinkedList) print() {
+	if linkedList.len == 0 {
+		return
+	}
 
+	curNode := linkedList.head
+	fmt.Printf("%d -> ", curNode.value)
+
+	for curNode.next != nil {
+		curNode = curNode.next
+		fmt.Printf("%d -> ", curNode.value)
+	}
+
+	fmt.Println()
 }
 
 func main() {
 	linkedList := &LinkedList{}
 
 	linkedList.add(5)
+	linkedList.add(7)
+	linkedList.add(8)
+
+	linkedList.print()
+	linkedList.remove(7)
+	linkedList.remove(8)
+
+	linkedList.print()
 }
